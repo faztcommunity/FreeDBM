d2df1e41ede972e528c37f78d9f0bd77ef1d353e
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-07-31 01:25:30 -0400

    feat(app): Added main application settings

---
+++ src/app/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
+
+from flask import Flask
+from flask_restful import Api
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
+from app.models import db
+import config
+
+app = Flask(__name__)
+
+# TODO(jsgonzlez661): Load development mode
+app.config.from_object(config.DevelopmentConfig)
+
+CORS(app)  # TODO(jsgonzlez661): Configure cross-origin resource sharing
+
+api = Api(app)
+migrate = Migrate(app, db)  # TODO(jsgonzlez661): Flask-Migrate Configuration
+
+manager = Manager(app)  # TODO(jsgonzlez661): Flask-Script Configuration
+manager.add_command('db', MigrateCommand)
+
+from .routes.signup import Signup
+# TODO(jsgonzlez661): Register resource signup
+api.add_resource(Signup, '/v1/auth/signup')
