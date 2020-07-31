530b4055b313e4219950cdaed129ed3fcdf4f190
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-07-31 01:08:48 -0400

    style(config): Modified format according to PEP8

--- src/config.py
+++ src/config.py
@@ -1,17 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 
 # TODO(jsgonzlez661): Configuration file for development or production mode
 
-KEY = b"k\xec\xa6@\x8b7'\xb8\x0c>9\xd9\xcc\xc2 \xb5"	
+KEY = b"k\xec\xa6@\x8b7'\xb8\x0c>9\xd9\xcc\xc2 \xb5"
 
-class BaseConfig(object): # TODO(jsgonzlez661): Basic mode settings
-	SECRET_KEY = KEY
-	SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-class DevelopmentConfig(BaseConfig): # TODO(jsgonzlez661): Configuration for the development mode
-	SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:@127.0.0.1:3306/login_token' # TODO(jsgonzlez661): URI database for development 
-	DEBUG = True
+class BaseConfig(object):  # TODO(jsgonzlez661): Basic mode settings
+    SECRET_KEY = KEY
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-class ProductionConfig(BaseConfig): # TODO(jsgonzlez661): Configuration for the production mode
-	SQLALCHEMY_DATABASE_URI = '' # TODO(jsgonzlez661): URI database for production 
-	DEBUG = False
+
+# TODO(jsgonzlez661): Configuration for the development mode
+class DevelopmentConfig(BaseConfig):
+    # TODO(jsgonzlez661): URI database for development
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:@127.0.0.1:3306/freedbm_data'
+    DEBUG = True
+
+
+# TODO(jsgonzlez661): Configuration for the production mode
+class ProductionConfig(BaseConfig):
+    # TODO(jsgonzlez661): URI database for production
+    SQLALCHEMY_DATABASE_URI = ''
+    DEBUG = False
