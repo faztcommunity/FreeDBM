a5e9841e1b22bdf5e4ac20b97fc90816bee59741
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-07-31 01:15:36 -0400

    feat(run): Added migration management

--- src/run.py
+++ src/run.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 
 # TODO(jsgonzlez661): File to start the server
 
-from app import app 
-from app.models import db  
+from app import app, manager
+from app.models import db
 
 if __name__ == '__main__':
-	db.init_app(app) 
-	with app.app_context():
-		db.create_all()
-	app.run()  
+    db.init_app(app)  # TODO(jsgonzlez661): Load database in app
+    # with app.app_context():
+    # ....db.create_all() # TODO(jsgonzlez661): Make all tables
+    manager.run() # TODO(jsgonzlez661): Migration configuration 
