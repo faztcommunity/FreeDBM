b733dc27bbbe978ed261caa2151cb319c8567487
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-07-31 01:17:25 -0400

    style(run): Modified format according to PEP8

--- src/run.py
+++ src/run.py
@@ -8,7 +8,9 @@ from app import app, manager
 from app.models import db
 
 if __name__ == '__main__':
+
     db.init_app(app)  # TODO(jsgonzlez661): Load database in app
     # with app.app_context():
     # ....db.create_all() # TODO(jsgonzlez661): Make all tables
-    manager.run() # TODO(jsgonzlez661): Migration configuration 
+
+    manager.run()  # TODO(jsgonzlez661): Migration configuration
