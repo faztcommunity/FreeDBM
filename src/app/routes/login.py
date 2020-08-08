f100f58d8ddf7f5542cdef9f4fc18ab1a2ad9473
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-08-07 23:41:03 -0400

    feat(route): Add method GET in route

--- src/app/routes/login.py
+++ src/app/routes/login.py
@@ -7,23 +7,15 @@ from app.routes import *
 
 class Login(Resource):
 
-    def post(self):
-        json_file = request.get_json(force=True)
-
-        if(Responds.validate_json_login(json_file)):
-            auth_token = json_file.get('auth_token', None)
+    def get(self):  # TODO(jsgonzlez661): Method GET for route Login
+        auth_token = request.args.get('auth_token', None)
+        if(auth_token != None):
             try:
-                # TODO(jsgonzlez661): Validate token
-                decoded = jwt.decode(auth_token, config.KEY, algorithm='HS256')
-            except jwt.exceptions.DecodeError:
-                # TODO(jsgonzlez661): If token no validate
-                return JSON.load_json('error_token')
-            except jwt.ExpiredSignatureError:
-                # TODO(jsgonzlez661): Check if token expire
-                return JSON.load_json('error_token_expire'), 401
-
-            email = decoded['user_data']['email']
-            password = decoded['user_data']['password'].encode('utf-8')
+                decoded = Login.validate_token(auth_token)
+                email = decoded['user_data']['email']
+                password = decoded['user_data']['password'].encode('utf-8')
+            except:
+                return decoded
 
             if(User.check_password(email, password)):  # TODO(jsgonzlez661): Check user's credentials
                 responds = {
@@ -37,3 +29,42 @@ class Login(Resource):
 
             return JSON.load_json('error_email_password')
         return JSON.load_json('error_data')
+
+    def post(self):  # TODO(jsgonzlez661): Method POST for route Login
+        json_file = request.get_json(force=True)
+
+        if(Responds.validate_json(json_file, 'login')):
+            auth_token = json_file.get('auth_token', None)
+            try:
+                decoded = Login.validate_token(auth_token)
+                email = decoded['user_data']['email']
+                password = decoded['user_data']['password'].encode('utf-8')
+            except:
+                return decoded
+
+            if(User.check_password(email, password)):  # TODO(jsgonzlez661): Check user's credentials
+                responds = {
+                    "responds": JSON.load_json('success_login'),
+                    "auth_token": auth_token
+                }
+                cookie = make_response(responds)
+                # TODO(jsgonzlez661): Make Cookie
+                cookie.set_cookie("auth_token", auth_token)
+                return cookie
+
+            return JSON.load_json('error_email_password')
+        return JSON.load_json('error_data')
+
+    @classmethod
+    # TODO(jsgonzlez661): Validate token
+    def validate_token(cls, auth_token):
+        try:
+            decoded = jwt.decode(auth_token, config.KEY, algorithm='HS256')
+        except jwt.exceptions.DecodeError:
+            # TODO(jsgonzlez661): If token no validate
+            return JSON.load_json('error_token')
+        except jwt.ExpiredSignatureError:
+            # TODO(jsgonzlez661): Check if token expire
+            return JSON.load_json('error_token_expire'), 401
+
+        return decoded
--- src/app/routes/signup.py
+++ src/app/routes/signup.py
@@ -7,24 +7,31 @@ from app.routes import *
 
 class Signup(Resource):
 
+    def get(self):  # TODO(jsgonzlez661): Method GET for route Signup
+        # TODO(jsgonzlez661): Get params data user
+        username = request.args.get('username', None)
+        email = request.args.get('email', None)
+        password = request.args.get('password', None)
+        if(username != None and email != None and password != None):
+            return Signup.user_email_check(username, email, password)
+        return JSON.load_json('error_data'), 400
+
     def post(self):  # TODO(jsgonzlez661): Method POST for route Signup
         # TODO(jsgonzlez661): Load file json of request
         json_file = request.get_json(force=True)
-
-        if(Responds.validate_json_signup(json_file)):
+        if(Responds.validate_json(json_file, 'user')):
             username = json_file.get('username', None)
             email = json_file.get('email', None)
             password = json_file.get('password', None)
+            return Signup.user_email_check(username, email, password)
+        return JSON.load_json('error_data'), 400
 
-            if(User.get_by_email(email)):
-                # TODO(jsgonzlez661): Return message error email
-                return JSON.load_json('error_email'), 401
-            elif(User.get_by_username(username)):
-                # TODO(jsgonzlez661): Check username unique
-                return JSON.load_json('error_username'), 401
-            else:
-                # TODO(jsgonzlez661): Add user in DB
-                User.create_element(username, email, password)
-                return Responds.get_message_signup(json_file), 201
-
-        return error_data_schema, 400
+    @classmethod  # TODO(jsgonzlez661): Check data user
+    def user_email_check(cls, username, email, password):
+        if(User.get_by_email(email)):  # TODO(jsgonzlez661): Return message error email
+            return JSON.load_json('error_email'), 401
+        elif(User.get_by_username(username)):  # TODO(jsgonzlez661): Check username unique
+            return JSON.load_json('error_username'), 401
+        else:  # TODO(jsgonzlez661): Add user in DB
+            User.create_element(username, email, password)
+            return Responds.get_message_signup(username, email, password), 201
