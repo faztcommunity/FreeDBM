5db5d552f33effdecc86c81373fb1abf1fc62719
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-08-07 23:42:58 -0400

    refactor(__init__): Code optimization

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -18,7 +18,7 @@ import datetime
 class Responds():
 
     @classmethod
-    def get_message_signup(cls, json_file):
+    def post_message_signup(cls, json_file):
         file_json = {
             'user_data': json_file,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=86400)
@@ -33,22 +33,29 @@ class Responds():
         return responds
 
     @classmethod
-    # TODO(jsgonzlez661): Validate schema json for signup
-    def validate_json_signup(cls, json_file):
-        v = Validator()
-        return v.validate(json_file, JSON.load_json('user'))
+    def get_message_signup(cls, username, email, password):
+        file_json = {
+            'user_data': {
+                'username': username,
+                'email': email,
+                'password': password
+            },
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=86400)
+        }
+        # TODO(jsgonzlez661): Time expiration for 24 hours
+        encoded_jwt = jwt.encode(file_json, config.KEY, algorithm='HS256')
+        responds = {
+            # TODO(jsgonzlez661): Make Token
+            "auth_token": encoded_jwt.decode("utf-8"),
+            "responds": JSON.load_json('success_signup')
+        }
+        return responds
 
     @classmethod
-    # TODO(jsgonzlez661): Validate schema json for login
-    def validate_json_login(cls, json_file):
+    # TODO(jsgonzlez661): Validate schema json
+    def validate_json(cls, json_file, name_file):
         v = Validator()
-        return v.validate(json_file, JSON.load_json('login'))
-
-    @classmethod
-    # TODO(jsgonzlez661): Validate schema json for get new token
-    def validate_json_token(cls, json_file):
-        v = Validator()
-        return v.validate(json_file, JSON.load_json('user_token'))
+        return v.validate(json_file, JSON.load_json(name_file))
 
     @classmethod
     # TODO(jsgonzlez661): Generate new token
