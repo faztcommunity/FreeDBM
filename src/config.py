762d6f407c9d9e74cf16e73e36bd2422e4e61290
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-08-14 00:05:59 -0400

    feat(app): Update

--- src/config.py
+++ src/config.py
@@ -11,6 +11,16 @@ class BaseConfig(object):  # TODO(jsgonzlez661): Basic mode settings
     SECRET_KEY = KEY
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+    # TODO(jsgonzlez661): Configuration for Flask-Mail send mail
+    MAIL_SERVER = 'smtp.gmail.com'  # TODO(jsgonzlez661): Mail Server Here
+    MAIL_PORT = 587
+    MAIL_USERNAME = 'email'  # TODO(jsgonzlez661): Email Here
+    MAIL_PASSWORD = 'password'  # TODO(jsgonzlez661): Password Here
+    # TODO(jsgonzlez661): Username and email Here
+    DONT_REPLY_FROM_EMAIL = '(Username, email)'
+    ADMINS = ('email', )  # TODO(jsgonzlez661): Email Here
+    MAIL_USE_TLS = True
+
 
 # TODO(jsgonzlez661): Configuration for the development mode
 class DevelopmentConfig(BaseConfig):
