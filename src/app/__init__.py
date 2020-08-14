d2f63ede7ac297f999c542532c65657b68019602
Jose Gonzalez <jsgonzlez661@gmail.com>
2020-08-14 00:03:41 -0400

    feat(app): Add new routes

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -7,6 +7,7 @@ from flask_restful import Api
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
+from flask_mail import Mail
 from app.models import db
 import config
 
@@ -23,12 +24,21 @@ migrate = Migrate(app, db)  # TODO(jsgonzlez661): Flask-Migrate Configuration
 manager = Manager(app)  # TODO(jsgonzlez661): Flask-Script Configuration
 manager.add_command('db', MigrateCommand)
 
+mail = Mail(app)  # TODO(jsgonzlez661): Flask-Mail
+
 from .routes.signup import Signup
 from .routes.login import Login
 from .routes.token import Token
 from .routes.logout import Logout
+from .routes.change_password import ChangePassword
+from .routes.recovery import RecoveryPassword
+from .routes.reset import ResetPassword
+
 # TODO(jsgonzlez661): Register resource signup
 api.add_resource(Signup, '/v1/auth/signup')
 api.add_resource(Login, '/v1/auth/login')
 api.add_resource(Token, '/v1/auth/token')
 api.add_resource(Logout, '/v1/auth/logout')
+api.add_resource(ChangePassword, '/v1/auth/user/new_password')
+api.add_resource(RecoveryPassword, '/v1/auth/user/recovery')
+api.add_resource(ResetPassword, '/v1/auth/user/reset')
